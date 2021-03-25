7ed4127a10dfe0110b48be6b6ef8cf5800620666
Serena Moon <shymagenta16@gmail.com>
2021-03-25 06:36:33 -0600

    editing css mostly

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,66 @@
 
 var xhr = new XMLHttpRequest();
-var url = "https://ghibliapi.herokuapp.com/films"
+var url = "https://ghibliapi.herokuapp.com/films?&count=4"
 
-xhr.open("GET", url, true);
-
+xhr.open('GET', url, true);
 xhr.send(null);
 
 xhr.onload = function(){
-
 	if(xhr.status == 200){
-		//document.write(xhr.responseText)
 
 		var ghibliData = JSON.parse(xhr.responseText)
-
-		var otitle = ghibliData[0].original_title;
-
-		$('body').text(otitle)
-
 		console.log(ghibliData)
 
-		// $("body").prepend("<img src='" + ghibliData.url + "'>")
+		ghibliData.forEach(function(data){
+
+			$(".pod").prepend("<div>" + "<h2>" + ghibliData.title + ghibliData.original_title + ghibliData.original_title_romanized + "</h2><h3>" + ghibliData.director + ghibliData.producer + ghibliData.release_date + "</h3><p>" + ghibliData.description + "</p></div>");
+
+		// var title = ghibliData[0].original_title;
+
+		// $("p").text(title)
+	})
 	}
-
-
 }
+
+// var baseUrl = "https://ghibliapi.herokuapp.com/films&count=42"
+// var url = "https://ghibliapi.herokuapp.com/films&count=42"
+// var query = "&count=42"
+// // var url = baseUrl + query
+
+
+// // $("#submit").on("click",function(){
+// // 	query = "&count" + $("#myInput").val();
+// // 	callAPI();
+// // })
+
+// // function callAPI() {
+
+// 	// var url = baseUrl + query;
+
+// 	// xhr.open("GET", url, true);
+
+// 	// xhr.send(null);
+
+// xhr.onload = function(){
+
+// 	if(xhr.status == 200){
+// 		//document.write(xhr.responseText)
+
+// 		var ghibliData = JSON.parse(xhr.responseText)
+
+// 		var otitle = ghibliData[0].original_title;
+
+// 		xhr.open("GET", url, true);
+
+// 		xhr.send(null);
+
+// 		$('body').text(otitle)
+
+// 		// $("p").append("<h2>" + ghibliData + "</h2>")
+
+// 		console.log(ghibliData)
+
+// 		// $("body").prepend("<img src='" + ghibliData.url + "'>")
+// 	}
+// }
+// }
